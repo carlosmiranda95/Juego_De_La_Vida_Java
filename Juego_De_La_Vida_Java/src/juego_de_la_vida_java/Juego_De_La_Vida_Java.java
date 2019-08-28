75fb162b357d971806fd9ca59aa2f5083c2b7d5f
Carlos Roberto Miranda Rocha <carlosmiranda2295@gmail.com>
2019-08-28 15:06:48 -0400

    Primera Prueba Fallida

---
+++ Juego_De_La_Vida_Java/src/juego_de_la_vida_java/Juego_De_La_Vida_Java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juego_de_la_vida_java;
+
+/**
+ *
+ * @author Carlos Miranda Rocha
+ */
+public class Juego_De_La_Vida_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Juego_De_La_Vida_Java/test/Juego_de_la_vida_java_test.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Carlos Miranda Rocha
+ */
+public class Juego_de_la_vida_java_test {
+    
+    public Juego_de_la_vida_java_test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void testCrearTablero(){
+        int row = 1;
+        int col = 2;
+        boolean respuesta = Juego_De_La_Vida_Java.crearTabla(row,col);
+        assertEquals(true, respuesta);
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
